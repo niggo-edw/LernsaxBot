57aa1111775236ef7a183ab66d8365e4e89dc0ef
niggo-edw <wunderwitz@gmx.net>
2020-09-06 01:15:28 +0200

    Added support for sending files

--- saxophon.py
+++ saxophon.py
@@ -14,12 +14,15 @@ passwort_field = '//*[@id="login_password"]'
 passwort = 'D131/2LdKBL'
 loginButton = '//*[@id="a745761"]/input[4]'
 email_link = '//*[@id="menu_105592"]'
+upload = '//*[@id="main_content"]/table[5]/tbody/tr/td[3]/input'
 
 recipient = 'anselm.noetzold@lghe.lernsax.de'
-subject = 'lernsax-spambot - du bist mein versuchskaninchen'
-text = 'automatik test looped v2 #'
+subject = 'aber gerne doch'
+text = 'ich kann jetzt auch dateien anhängen. gib mir einfach ne zahl, nen betreff, nen text und optional noch n bild oder so, dann wird der herr professor vollgespamt'
 
-#öffnet lernsax
+sendFile = 'true'
+
+#öffnet den mail-bereich von lernsax
 driver.get('https://www.lernsax.de/wws/9.php#/wws/100001.php?sid=73686923944239016459925482550630S1adb4d16')
 driver.switch_to.frame(driver.find_element_by_id('main_frame'))
 driver.find_element_by_xpath(username_field).send_keys(username)
@@ -27,7 +30,8 @@ driver.find_element_by_xpath(passwort_field).send_keys(passwort)
 driver.find_element_by_xpath(loginButton).click()
 driver.find_element_by_link_text('E-Mail').click()
 
-for i in range(5):
+#verschickt mails
+for i in range(3):
     driver.find_element_by_link_text('E-Mail schreiben').click()
     time.sleep(0.1)
     driver.switch_to.default_content()
@@ -35,6 +39,12 @@ for i in range(5):
     driver.find_element_by_id('to').send_keys(recipient)
     driver.find_element_by_id('subject').send_keys(subject)
     driver.find_element_by_id('body').send_keys(text + str(i))
+
+    if (sendFile == 'true'):
+        fileloc = 'C:/Users/Anwender/Documents/06 Programming/09lernsax-bot/foto.jpg'
+        driver.find_element_by_id('file').send_keys(fileloc)
+        element = driver.find_element_by_xpath(upload).click()
+
     driver.find_element_by_name('send_mail').click()
     driver.execute_script('return ww.close();')
     driver.switch_to.default_content()
